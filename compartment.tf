1c9b84fbcc35f4d75c6a9570ed4f0880b506120b
Robin Lieb <robin.j.lieb@gmail.com>
2022-12-02 17:23:38 +0100

    fix: typo in resource

--- compartment.tf
+++ compartment.tf
@@ -1,4 +1,4 @@
-resource "oci_identity_compartmet" "terraform_compartment" {
+resource "oci_identity_compartment" "terraform_compartment" {
     compartment_id = var.tenancy_ocid
     description = "Compartment for Terraform resources."
     name = "terraform"
