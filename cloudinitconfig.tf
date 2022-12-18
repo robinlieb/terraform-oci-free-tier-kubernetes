b83d3ce380809852e198465985371f5308362ecb
Robin Lieb <robin.j.lieb@gmail.com>
2022-12-18 11:08:32 +0100

    feat: added kubeadm token module

--- cloudinitconfig.tf
+++ cloudinitconfig.tf
@@ -3,6 +3,11 @@ data "cloudinit_config" "config" {
   part {
     content_type = "text/cloud-config"
     filename     = "base.yaml"
-    content      = templatefile("./cloud-config.tftpl", { node = count.index })
+    content      = templatefile("./cloud-config.tftpl", { node = count.index, token = module.kubeadm_token.token })
   }
+}
+
+module "kubeadm_token" {
+  source  = "robinlieb/kubeadm-token/random"
+  version = "1.0.0"
 }
