593d38193a185d051734de12a23a4e9e27e80b23
Robin Lieb <robin.j.lieb@gmail.com>
2023-03-01 16:40:20 +0100

    refacctor: combined subnets and security list in files

--- securitylists.tf
+++ securitylists.tf
@@ -62,3 +62,25 @@ resource "oci_core_security_list" "public_security_list" {
     }
   }
 }
+
+resource "oci_core_security_list" "private_security_list" {
+  compartment_id = oci_identity_compartment.terraform_compartment.id
+  vcn_id         = module.vcn.vcn_id
+
+  display_name = "security-list-for-private-subnet"
+
+  egress_security_rules {
+    stateless        = false
+    destination      = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+    protocol         = "all"
+  }
+
+
+  ingress_security_rules {
+    stateless   = false
+    source      = "10.0.0.0/16"
+    source_type = "CIDR_BLOCK"
+    protocol    = "all"
+  }
+}
--- subnets.tf
+++ subnets.tf
@@ -1,3 +1,13 @@
+resource "oci_core_subnet" "vcn_public_subnet" {
+  compartment_id = oci_identity_compartment.terraform_compartment.id
+  vcn_id         = module.vcn.vcn_id
+  cidr_block     = "10.0.0.0/24"
+
+  display_name      = "public-subnet"
+  security_list_ids = [oci_core_security_list.public_security_list.id]
+  route_table_id    = module.vcn.ig_route_id
+}
+
 resource "oci_core_subnet" "vcn_private_subnet" {
   compartment_id = oci_identity_compartment.terraform_compartment.id
   vcn_id         = module.vcn.vcn_id
