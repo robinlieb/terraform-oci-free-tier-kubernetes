63dfdefb940e7b7a997375b8c2554556d01b0d27
Robin Lieb <robin.j.lieb@gmail.com>
2023-03-05 19:23:12 +0100

    feat: added description to variables and outputs

--- outputs.tf
+++ outputs.tf
@@ -1,75 +1,94 @@
 output "all_availability_domains" {
-  value = data.oci_identity_availability_domains.ads.availability_domains
+  value       = data.oci_identity_availability_domains.ads.availability_domains
+  description = "All availaility domains."
 }
 
 output "compartment_name" {
-  value = oci_identity_compartment.terraform_compartment.name
+  value       = oci_identity_compartment.terraform_compartment.name
+  description = "Name of the compartment."
 }
 
 output "compartment_id" {
-  value = oci_identity_compartment.terraform_compartment.id
+  value       = oci_identity_compartment.terraform_compartment.id
+  description = "ID of the compartment."
 }
 
 output "vcp_id" {
-  value = module.vcn.vcn_id
+  value       = module.vcn.vcn_id
+  description = "ID of the VCN."
 }
 
 output "vcn_id_route_id" {
-  value = module.vcn.ig_route_id
+  value       = module.vcn.ig_route_id
+  description = "ID of the route."
 }
 
 output "vcn_nat_gateway_id" {
-  value = module.vcn.nat_gateway_id
+  value       = module.vcn.nat_gateway_id
+  description = "ID of the NAT gateway."
 }
 
 output "vcn_nat_route_id" {
-  value = module.vcn.nat_route_id
+  value       = module.vcn.nat_route_id
+  description = "ID of the NAT route."
 }
 
 output "available_images" {
-  value = data.oci_core_images.instance_images.images
+  value       = data.oci_core_images.instance_images.images
+  description = "Available images."
 }
 
 output "public_ip_for_compute_instance" {
-  value = oci_core_instance.ubuntu_instance[*].public_ip
+  value       = oci_core_instance.ubuntu_instance[*].public_ip
+  description = "Public IPs of the instances."
 }
 
 output "instance_name" {
-  value = oci_core_instance.ubuntu_instance[*].display_name
+  value       = oci_core_instance.ubuntu_instance[*].display_name
+  description = "Names of the instances."
 }
 
 output "instance_ocid" {
-  value = oci_core_instance.ubuntu_instance[*].id
+  value       = oci_core_instance.ubuntu_instance[*].id
+  description = "OCID of the instances."
 }
 
 output "instance_region" {
-  value = oci_core_instance.ubuntu_instance[*].region
+  value       = oci_core_instance.ubuntu_instance[*].region
+  description = "Region of the instances."
 }
 
 output "instance_shape" {
-  value = oci_core_instance.ubuntu_instance[*].shape
+  value       = oci_core_instance.ubuntu_instance[*].shape
+  description = "Shape of the instances."
 }
 
 output "instance_state" {
-  value = oci_core_instance.ubuntu_instance[*].state
+  value       = oci_core_instance.ubuntu_instance[*].state
+  description = "State of the instances."
 }
 
 output "instance_OCPUs" {
-  value = oci_core_instance.ubuntu_instance[*].shape_config[0].ocpus
+  value       = oci_core_instance.ubuntu_instance[*].shape_config[0].ocpus
+  description = "CPUs of the instances."
 }
 
 output "instance_memory_in_GBs" {
-  value = oci_core_instance.ubuntu_instance[*].shape_config[0].memory_in_gbs
+  value       = oci_core_instance.ubuntu_instance[*].shape_config[0].memory_in_gbs
+  description = "Memory in GB of the instances."
 }
 
 output "time_created" {
-  value = oci_core_instance.ubuntu_instance[*].time_created
+  value       = oci_core_instance.ubuntu_instance[*].time_created
+  description = "Creation time of the instances"
 }
 
 output "kubeconfig" {
-  value = module.kubeconfig.kubeconfig
+  value       = module.kubeconfig.kubeconfig
+  description = "Kubeconfig to access the cluster"
 }
 
 output "kubeconfig_commands" {
-  value = module.kubeconfig.kubeconfig_commands
+  value       = module.kubeconfig.kubeconfig_commands
+  description = "Kubeconfig commands to apply to local kubeconfig"
 }
--- variables.tf
+++ variables.tf
@@ -1,55 +1,69 @@
 variable "tenancy_ocid" {
+  description = "The OCID of the tenany, e.g. ocid1.tenancy.oc1..aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa."
+  type        = string
 }
 
 variable "user_ocid" {
+  description = "The OCID of the user, e.g. ocid1.user.oc1..aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa."
+  type        = string
 }
 
 variable "private_key" {
+  description = "The private key retrieved from the Oracle account."
+  type        = string
 }
 
 variable "fingerprint" {
+  description = "The fingerprint of the private_key, e.g. 8c:bf:17:7b:5f:e0:7d:13:75:11:d6:39:0d:e2:84:74."
+  type        = string
 }
 
 variable "region" {
+  description = "The desired region of the cluster, e.g. us-phoenix-1."
+  type        = string
 }
 
 variable "ssh_public_key" {
+  description = "The public key used for SSH to the instances."
+  type        = string
 }
 
 variable "ssh_private_key" {
+  description = "The private key used for SSH to the instances."
+  type        = string
 }
 
 variable "name" {
-  description = "Name will be used for compartment and kubeconfig"
+  description = "Name will be used for compartment and kubeconfig."
   default     = "terraform-oci-free-tier-kubernetes"
 }
 
 variable "instance_shape" {
-  description = "Instance Shape"
+  description = "Shape of the instances."
   default     = "VM.Standard.A1.Flex"
 }
 
 variable "instance_os" {
-  description = "Operating system for compute instances"
+  description = "Operating system for compute instances."
   default     = "Canonical Ubuntu"
 }
 
 variable "linux_os_version" {
-  description = "Operating system version for all Linux instances"
+  description = "Operating system version for all Linux instances."
   default     = "22.04"
 }
 
 variable "instance_ocpus" {
-  description = "Number of OCPUs available to the instance"
+  description = "Number of OCPUs available to the instance."
   default     = 1
 }
 
 variable "instance_memory" {
-  description = "Amount of memory available to the instance, in gigabytes"
+  description = "Amount of memory available to the instance, in gigabytes."
   default     = 6
 }
 
 variable "instance_count" {
-  description = "Number of instances which should be created"
+  description = "Number of instances which should be created."
   default     = 4
 }
