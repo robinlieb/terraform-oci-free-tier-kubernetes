5c4e328428424206913a75813b38b6bec622ed39
Robin Lieb <robin.j.lieb@gmail.com>
2023-05-18 16:36:40 +0200

    fix: network interface name (#2)

--- utils/init.sh
+++ utils/init.sh
@@ -2,5 +2,5 @@
 
 set -euxo pipefail
 
-node_ip=$(ip -o -4 addr list enp0s3 | awk '{print $4}' | cut -d/ -f1)
+node_ip=$(ip -o -4 addr list enp0s6 | awk '{print $4}' | cut -d/ -f1)
 echo KUBELET_EXTRA_ARGS="--node-ip $node_ip --cloud-provider=external --provider-id=${instanceID}" | sudo tee /etc/default/kubelet
