3ccb32a1cc996318beb8fdd0ad01c6a8757af1c6
Robin Lieb <robin.j.lieb@gmail.com>
2023-11-08 20:33:40 +0100

    fix: replace deprecated template provider

--- modules/kubeconfig/outputs.tf
+++ modules/kubeconfig/outputs.tf
@@ -1,7 +1,23 @@
 output "kubeconfig_commands" {
-  value = data.template_file.kubeconfig_commands.rendered
+  value = templatefile("${path.module}/kubeconfig_commands.tftpl", {
+    cluster_name     = var.cluster_name,
+    cluster_url      = var.cluster_url,
+    user_name        = var.user_name,
+    context_name     = var.context_name,
+    ca_file          = base64encode(var.ca_file),
+    client_cert_file = base64encode(var.client_cert_file),
+    client_key_file  = base64encode(var.client_key_file)
+  })
 }
 
 output "kubeconfig" {
-  value = data.template_file.kubeconfig.rendered
+  value = templatefile("${path.module}/kubeconfig.tftpl", {
+    cluster_name     = var.cluster_name,
+    cluster_url      = var.cluster_url,
+    user_name        = var.user_name,
+    context_name     = var.context_name,
+    ca_file          = base64encode(var.ca_file),
+    client_cert_file = base64encode(var.client_cert_file),
+    client_key_file  = base64encode(var.client_key_file)
+  })
 }
